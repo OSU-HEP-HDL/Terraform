2da53c37c1bef7b88fd24841bb6361eb45e3fdd4
Jacob Edwin Crosby <jacob.edwin.crosby@cern.ch>
2024-11-22 15:27:18 -0600

    separted vars

--- provider.tf
+++ provider.tf
@@ -26,17 +26,6 @@ terraform {
   }
 }
 
-variable "proxmox_api_url" {
-    type = string
-}
-
-variable "proxmox_api_token_id" {
-    type = string
-}
-
-variable "proxmox_api_token_secret" {
-    type = string
-}
 
 provider "proxmox" {
 
